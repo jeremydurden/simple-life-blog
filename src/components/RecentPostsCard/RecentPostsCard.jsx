73eb08d3f3afbca6cf33279df57f3fa8914d410b
Jeremy Durden <jeremydurden@gmail.com>
2022-04-24 11:58:51 -0700

    removing the Recent Posts sidebar card from mobile version of recent posts page

--- src/components/RecentPostsCard/RecentPostsCard.jsx
+++ src/components/RecentPostsCard/RecentPostsCard.jsx
@@ -5,9 +5,18 @@ import food from "../../images/food.jpg";
 import deco from "../../images/deco.jpg";
 import work from "../../images/work.jpg";
 
+import { useLocation } from "react-router-dom";
+
 const RecentPostsCard = () => {
+  const location = useLocation();
+  const show = location.pathname === "/recent";
+
   return (
-    <div className="recent-posts__container">
+    <div
+      className={`recent-posts__container ${
+        show ? "recent-posts__container--hide" : ""
+      }`}
+    >
       <h1 className="recent-posts__title">Recent Posts</h1>
       <RecentPosts photo={food} text="Keep cooking simple" />
 
