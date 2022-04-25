31e5d9a02a1a70f7cca44683412615f953b112fd
Jeremy Durden <jeremydurden@gmail.com>
2022-04-25 12:19:06 -0700

    correcting a typo in data.js

--- src/data/data.js
+++ src/data/data.js
@@ -6,7 +6,7 @@ import deco from "../images/deco.jpg";
 const data = [
   {
     photo: life,
-    date: "July 19, 20203",
+    date: "July 19, 2023",
     title: "Finding Simplicity in life",
     comments: "3",
     text: "Life can get complicated really quickly, but it doesn't have to be! There are many ways to simplify your life, a few of which we've explored in the past. This week we're taking a bit of a approach though, in how you can find simplicity in the life you already living.",
@@ -14,7 +14,7 @@ const data = [
   },
   {
     photo: food,
-    date: "July 12, 20203",
+    date: "July 12, 2023",
     title: "Keeping cooking simple",
     comments: "4",
     text: "Food is a very important part of everyone's life. If you want to be healthy, you have to eat healthy. One of the easiest ways to do that is to keep your cooking nice and simple.",
@@ -22,7 +22,7 @@ const data = [
   },
   {
     photo: work,
-    date: "July 5, 20203",
+    date: "July 5, 2023",
     title: "Simplicity and work",
     comments: "1",
     text: "Work is often a major source of stress. People get frustrated, it ruins their relationship with others and it leads to burnout. By keeping your work life as simple as possible, it will help balance everything out. ",
@@ -30,7 +30,7 @@ const data = [
   },
   {
     photo: deco,
-    date: "June 28, 20203",
+    date: "June 28, 2023",
     title: "Simple Decorations",
     comments: "5",
     text: "A home isn't a home until you've decorated a little. People either don't decorate, or they go overboard and it doesn't have the impact they were hoping for. Staying simple will help draw the eye where you want it to and make things pop like never before. ",
